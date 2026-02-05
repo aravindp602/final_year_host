c55d2d5ee44de14ba026e4160674fa868fd9ca42
Aravind P <121235327+aravindp602@users.noreply.github.com>
2026-02-05 08:13:47 +0530

    Update API_BASE_URL for production environment

--- electron-app/src/config.js
+++ electron-app/src/config.js
@@ -18,4 +18,4 @@ const isLocal =
  */
 export const API_BASE_URL = isLocal
   ? "http://localhost:5001"
-  : "https://mia-submissions-smaller-boutique.trycloudflare.com";
+  : "https://astronomy-prostores-cruises-via.trycloudflare.com";
